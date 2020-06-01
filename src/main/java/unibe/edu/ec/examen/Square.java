8673aaf3f2623021e3940d0fe3a018358f7e69e5
camil <camil@DESKTOP-4GCMOE7>
2020-06-01 11:19:51 -0500

    Int Square  Centimeter

---
+++ src/main/java/unibe/edu/ec/examen/Square.java
@@ -0,0 +1,20 @@
+package unibe.edu.ec.examen;
+
+public class Square {
+	private int centimeter;
+	
+	public Square(int centimeter) {
+		super();
+		this.centimeter = centimeter ;
+		
+	}
+
+	public int getCentimeter() {
+		return centimeter;
+	}
+
+	public void setCentimeter(int centimeter) {
+		this.centimeter = centimeter;
+	}
+
+}
